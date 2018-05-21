1ed34dc7e38085e3f8272dd419500709f8a22c84
Charles Yach <CharlesY712@gmail.com>
2018-05-21 10:20:12 -0600

    Creating photoData and making initial seed

---
+++ photoData.js
@@ -0,0 +1,16 @@
+const photoData = [
+  {
+    title: '2007 Rally WRX',
+    url: 'https://i.ytimg.com/vi/ZMSB50u9VjA/maxresdefault.jpg'
+  },
+  {
+    title: '2005 Rally WRX',
+    url: 'https://i.redd.it/k0g6d09smzaz.jpg'
+  },
+  {
+    title: '2006 Rally WRX',
+    url: 'https://i.redd.it/3hji3lfdhcfz.jpg'
+  }
+];
+
+module.exports = photoData;
