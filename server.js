24121157fc2105153f2e9e16283b871aef1395de
Charles Yach <CharlesY712@gmail.com>
2018-05-21 10:54:48 -0600

    Adding post request and verified working through Postman

--- server.js
+++ server.js
@@ -26,3 +26,23 @@ app.get('/api/v1/photos', (request, response) => {
       response.status(500).json({ error });
     });
 });
+
+app.post('/api/v1/photos', (req, res) => {
+  const photo = req.body;
+
+  for (let requiredParameter of ['title', 'url']) {
+    if (!photo[requiredParameter]) {
+      return res.status(422)
+        .send(`You are missing a ${requiredParameter}`);
+    }
+  }
+
+  database('photos').insert(photo, 'title')
+    .then(photoID => {
+      return res.status(201)
+        .json(`Successfully added ${photoID} to database.`);
+    })
+    .catch(err => {
+      return res.status(500).json({err});
+    });
+});
