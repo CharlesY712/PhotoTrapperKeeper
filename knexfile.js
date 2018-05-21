70c0b13d32703fa186024a17a08f727b2ccee788
Charles Yach <CharlesY712@gmail.com>
2018-05-21 09:47:29 -0600

    Initial migration

--- knexfile.js
+++ knexfile.js
@@ -3,10 +3,27 @@
 module.exports = {
 
   development: {
-    client: 'sqlite3',
-    connection: {
-      filename: './dev.sqlite3'
-    }
-  }
+    client: 'pg',
+    connection: 'postgres://localhost/photos',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
 
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/photos_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/test'
+    },
+    useNullAsDefault: true
+  }
+  
 };
